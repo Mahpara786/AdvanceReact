8a4aee8f478c3b2643982f660d3140ccb95e2be0
Mahpara786 <mahpara8604@gmail.com>
2021-08-10 18:28:11 +0530

    function

---
+++ src/tutorial/useStateErrorEx.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+function UseStateErrorEx() {
+    let title ="random title";
+    const handleClick = () => {
+        console.log(title);
+       //title = "My name is Obama";
+    }
+    return ( <React.Fragment>
+        <h2>{title}</h2>
+        <button type ="button" className="btn" onClick={handleClick} >
+            change title
+        </button>
+    </React.Fragment>
+        
+    )
+}
+
+export default UseStateErrorEx
