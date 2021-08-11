8dec6df1894d3ea3da9bcfabde9845084462a994
Mahpara786 <mahpara8604@gmail.com>
2021-08-11 17:15:11 +0530

    formsprintlistpage

--- src/tutorial/Forms.js
+++ src/tutorial/Forms.js
@@ -8,8 +8,9 @@ function Forms() {
     const handleSubmit = (e) =>{
         e.preventDefault();
         if(firstName && email) {
-            console.log('submit the value');    
-            const person = {firstName:firstName, email:email};
+            console.log('submit the value');
+            const person = {id : new Date().getTime().toString(),firstName,email};
+            console.log(person);    
     
         setPeople((people) => {
             return  [...people, person];
@@ -40,7 +41,7 @@ function Forms() {
         {people.map((person, index) => {
             const {id,firstName, email} = person;            
             return(
-                <div className = "item">
+                <div className = "item" key={id}>
                     <h4>{firstName}</h4>
                     <p>{email}</p>
                 </div>
