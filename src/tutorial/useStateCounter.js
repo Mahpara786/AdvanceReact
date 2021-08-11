bf54056aea8eb4827c62e7cb8c86bd44447dfe5a
Mahpara786 <mahpara8604@gmail.com>
2021-08-11 11:51:39 +0530

    complexCounterbutton

--- src/tutorial/useStateCounter.js
+++ src/tutorial/useStateCounter.js
@@ -2,6 +2,11 @@ import React, {useState} from 'react'
 
 function UseStateCounter() {
     const [value, setValue] = useState(0);
+    const complexIncrease = () => {
+        setTimeout(() => {
+            setValue(value+1);
+        }, 2000)
+    }
     return (
        <>
        <section style ={{margin: '4rem 0'}}>
@@ -10,6 +15,12 @@ function UseStateCounter() {
            <button className = 'btn' onClick={()=>setValue(value-1)}> decrease</button>
            <button className = 'btn' onClick={()=>setValue(value+1)}> increase</button>
            <button className = 'btn' onClick={()=>setValue(0)}> reset</button>
+       </section>
+       <section style={{margin: '4rem 0'}}>
+           <h2>A more Complex counter</h2>
+           <h1>{value}</h1>
+           <button className = 'btn' onClick={complexIncrease}> increase Later</button>
+
        </section>
        </>
     )
