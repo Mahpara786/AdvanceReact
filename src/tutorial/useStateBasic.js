bc71eee095b6a7e6ea8d0ca440a5c60bcc2dab0a
Mahpara786 <mahpara8604@gmail.com>
2021-08-11 09:22:03 +0530

    withsetstate

--- src/tutorial/useStateBasic.js
+++ src/tutorial/useStateBasic.js
@@ -2,10 +2,18 @@ import React, {useState} from 'react'
 
 
 function UseStateBasic() {
-    console.log(useState("Neeraj Chopra"));
+    const [day,setDay]=useState("Today is a rainy day :");
+    const handleClick = () => {
+        setDay("Tpday is a bright day!!")
+    };
     return (
         <div>
-            
+           <>
+           <h1>{day}</h1>
+           <button type ="button" className="btn" onClick={handleClick} >
+            change my day
+        </button>
+        </>
         </div>
     )
 }
