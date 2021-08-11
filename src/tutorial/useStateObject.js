b8fcd496624ceab1be1aa185f01503bd8073fe8e
Mahpara786 <mahpara8604@gmail.com>
2021-08-11 11:21:50 +0530

    secondCommitpassingobjecttofunction

--- src/tutorial/useStateObject.js
+++ src/tutorial/useStateObject.js
@@ -6,16 +6,23 @@ function UseStateObject() {
        age: 50,
        message: "I am a god",
    });
+   const [name,setName] = useState(
+       "Tony Stark")
+    const [age, setAge] = useState(45)
+    const [message,setMessage] = useState("I am IronMan")
+
+
    const changeMessage = () => {
        setPerson({...person, message:"not anymore"});
+       setMessage("HelloWorld");
    }
     return (
         <div>
         
         <>
-        <h3>{person.name}</h3>
-        <h3>{person.age}</h3>
-        <h3>{person.message}</h3>
+        <h3>{name}</h3>
+        <h3>{age}</h3>
+        <h3>{message}</h3>
         <button className="btn" onClick={changeMessage}> chnge my message</button>
         </>    
         </div>
