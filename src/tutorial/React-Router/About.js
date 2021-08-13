a059a41e8d666f2495e4bad025a1d8b4b45ea976
Mahpara786 <mahpara8604@gmail.com>
2021-08-13 10:39:26 +0530

    Learning RouterBasic

---
+++ src/tutorial/React-Router/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About() {
+    return (
+        <h4>This not about you</h4>
+    )
+}
+
+export default About
---
+++ src/tutorial/React-Router/Index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import About from './About';
+import Home from './Home';
+import Error from './Error';
+import NavBar from './NavBar';
+import People from './People';
+import Person from './Person';
+import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
+
+function ReactRouterDom() {
+    return (
+        <Router>
+        <NavBar />
+        <Switch>
+        <Route exact path="/">
+            <Home />
+        </Route>
+        <Route path="/about">
+        <About />
+        </Route> 
+        <Route path="/people">
+        <People/>
+        </Route> 
+        <Route path="*">
+            <Error />
+        </Route> 
+        </Switch>
+        </Router>
+        
+
+        
+       
+    )
+}
+export default ReactRouterDom
